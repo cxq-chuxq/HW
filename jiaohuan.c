91b183329373fbe9ff9ff23cd0696aafc016da4d
edz <1969309795@qq.com>
2020-02-28 09:07:04 +0800

    jiaohuan

---
+++ jiaohuan.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#define SHU 8
+int swap(int *a, int *b);
+
+int main() {
+    int a[SHU] = {0, 1, 2, 3, 4, 5, 6, 7};
+    time_t t;
+    srand(time(&t));
+
+
+    for (int i = 0; i < SHU; i++) {
+  
+        int l = rand() % 8;
+        swap(&a[l], &a[i]);
+    }
+
+    for (int i = 0; i < SHU; i++) {
+        printf("%3d", a[i]);
+    }
+
+    putchar('\n');
+    return 0;
+}
+
+int swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+    return *b;
+}
